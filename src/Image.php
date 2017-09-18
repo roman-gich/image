c11e8f5fe027b2fbe372dc82884750ba73a20762
Roman Gich <roman.gich@gmail.com>
2017-09-18 23:27:39 +0300

    basic class

---
+++ src/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Gich\Image;
+
+/**
+ * Description of Image
+ *
+ * @author Roman Gich <roman.gich@gmail.com>
+ */
+class Image {
+    
+    public function __construct() {
+        
+    }
+    
+    public function fromString($string) {
+        
+    }
+    
+    public function fromFile($path) {
+        
+    }
+    
+    public function rotate() {
+        
+    }
+    
+    public function opacity() {
+        
+    }
+    
+    
+    
+    
+}
